b56ce52693afa0d9d2019910ac946b95b24bebbf
Gilberto Pereira Sassi <gpsassi@live.com>
2021-05-24 16:54:14 -0300

    structure directory

--- scripts/structure-project.R
+++ scripts/structure-project.R
@@ -4,11 +4,10 @@ library(fs)
 dir_create(here("data", "raw"))
 dir_create(here("data", "processed"))
 dir_create(here(c(
-  "scripts",
   "figures",
   "output",
   "previous",
-  "paper",
-  "notes",
-  "documents"
-)))
+  "paper"
+  )
+  )
+  )
