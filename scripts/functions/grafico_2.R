480c54d88fea4de26e4f2c8141404bdfaecf06ed
Gilberto Pereira Sassi <gpsassi@live.com>
2021-06-07 16:43:04 -0300

    Building a template analysis, and I have added some essential references that I got with Carol.

--- scripts/functions/grafico_2.R
+++ scripts/functions/grafico_2.R
@@ -2,23 +2,23 @@
 #' 
 #' @param df a tibble 
 #' @param variavel_x a character (or factor) variable 
-#' @param variavel_y a character (or factor) variable
+#' @param variavel_fill a character (or factor) variable
 #' @param rotulo_x label of x axis
 #' @param rotulo_fill label of legend
 #' @param base directory to save the graphs
 #' 
 #' @return a \code{ggplot2} object 
 #' 
-#' @usage grafico_2(df, variavel_x, variavel_y, rotulo_x, rotulo_y)
+#' @usage grafico_2(df, variavel_x, variavel_fill, rotulo_x, rotulo_y)
 #' 
 #' @examples
 #' df <- tibble(x = sample(letters[1:5], 100, replace = T),
 #'              y = sample(letters[6:10], 100, replace = T))
 #' grafico_2(df, x, y, "x", "y")
-grafico_2 <- function(df, variavel_x, variavel_y, rotulo_x, rotulo_y = "", base = "") {
+grafico_2 <- function(df, variavel_x, variavel_fill, rotulo_x, rotulo_y = "", base = "") {
 
   g <- ggplot2::ggplot(df) +
-    ggplot2::geom_bar(aes_string(x = variavel_x, fill = variavel_y),
+    ggplot2::geom_bar(aes_string(x = variavel_x, fill = variavel_fill),
                       position = 'fill') +
     ggplot2::labs(x = rotulo_x, y = "Porcentabem") +
     ggplot2::scale_fill_discrete(rotulo_y) +
